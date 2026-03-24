09ca1a7c6f1df2ece04c3ab234b40e61c3526ca9
LopedeVega22 <felteza@gmail.com>
2026-03-24 11:39:34 +0100

    Se añaden las máquinas de páginas web en el main.tf de PRO correccion de espacios en PRE

--- Terraform grupal pre/main.tf
+++ Terraform grupal pre/main.tf
@@ -184,8 +184,8 @@ resource "azurerm_network_interface" "Clara-Campoamor-interface" {
   resource_group_name = local.rg_name
 
     ip_configuration {
-        name                         = "internal"
-        subnet_id                     = azurerm_subnet.subnet01-PRE.id
+        name = "internal"
+        subnet_id = azurerm_subnet.subnet01-PRE.id
         private_ip_address_allocation = "Static"
         private_ip_address = "10.0.1.100"
         public_ip_address_id = azurerm_public_ip.Clara-Campoamor-ip.id
