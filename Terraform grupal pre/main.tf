d9e337bf85fd676b6b2a7772d1ecb47fde65389a
LopedeVega22 <felteza@gmail.com>
2026-03-25 14:51:01 +0100

    Se descomentan las cosas que no se necesitaban comentar

--- Terraform grupal pre/main.tf
+++ Terraform grupal pre/main.tf
@@ -3,23 +3,6 @@ data "azurerm_resource_group" "rg_fct_ohmygit" {
   name = "RG_OhMyGit"
 }
 
-
-# Storage Account para el backend
-resource "azurerm_storage_account" "terratf" {
-  name                     = "terratfsa12345"
-  resource_group_name      = local.rg_name
-  location                 = local.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Contenedor para guardar el state
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.terratf.id
-}
-
-
 #Creamos la virtual network de PRO
 resource "azurerm_virtual_network" "virtual-network-PRE" {
   name                = "virtual-network-PRE" #llama al apartado nombre de virtual_network en los locals
@@ -172,137 +155,137 @@ resource "azurerm_virtual_machine" "Juana-de-Arco" {
   }
 }
 
-# #================================================
-# #Configuracion máquina Ubuntu server web frontend
-# #================================================
-# #IP publica
-# resource "azurerm_public_ip" "Clara-Campoamor-ip" {
-#   name                = "Clara-Campoamor-ip"
-#   resource_group_name = local.rg_name
-#   location            = local.location
-#   allocation_method   = "Static"
-#   tags = {
-#     environment = "PRE"
-#   }
-# }
+#================================================
+#Configuracion máquina Ubuntu server web frontend
+#================================================
+#IP publica
+resource "azurerm_public_ip" "Clara-Campoamor-ip" {
+  name                = "Clara-Campoamor-ip"
+  resource_group_name = local.rg_name
+  location            = local.location
+  allocation_method   = "Static"
+  tags = {
+    environment = "PRE"
+  }
+}
 
-# #network interface ubuntu server pre
-# resource "azurerm_network_interface" "Clara-Campoamor-interface" {
-#   name                = "Clara-Campoamor-interface"
-#   location            = local.location
-#   resource_group_name = local.rg_name
+#network interface ubuntu server pre
+resource "azurerm_network_interface" "Clara-Campoamor-interface" {
+  name                = "Clara-Campoamor-interface"
+  location            = local.location
+  resource_group_name = local.rg_name
 
-#     ip_configuration {
-#         name = "internal"
-#         subnet_id = azurerm_subnet.subnet01-PRE.id
-#         private_ip_address_allocation = "Static"
-#         private_ip_address = "10.0.1.100"
-#         public_ip_address_id = azurerm_public_ip.Clara-Campoamor-ip.id
-#   }
-# tags = {
-#     environment = "PRE"
-#   }
-# }
+    ip_configuration {
+        name = "internal"
+        subnet_id = azurerm_subnet.subnet01-PRE.id
+        private_ip_address_allocation = "Static"
+        private_ip_address = "10.0.1.100"
+        public_ip_address_id = azurerm_public_ip.Clara-Campoamor-ip.id
+  }
+tags = {
+    environment = "PRE"
+  }
+}
 
 
-# resource "azurerm_virtual_machine" "Clara-Campoamor" {
-#   name                  = "Clara-Campoamor"
-#   location              = local.location
-#   resource_group_name   = local.rg_name
-#   network_interface_ids = [azurerm_network_interface.Clara-Campoamor-interface.id]
-#   vm_size               = "Standard_B2as_v2"
+resource "azurerm_virtual_machine" "Clara-Campoamor" {
+  name                  = "Clara-Campoamor"
+  location              = local.location
+  resource_group_name   = local.rg_name
+  network_interface_ids = [azurerm_network_interface.Clara-Campoamor-interface.id]
+  vm_size               = "Standard_B2as_v2"
 
-# storage_image_reference {
-#    publisher = "canonical"
-#     offer     = "ubuntu-24_04-lts"
-#     sku       = "ubuntu-pro-gen1"
-#    version   = "latest"
-#   }
+storage_image_reference {
+   publisher = "canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "ubuntu-pro-gen1"
+   version   = "latest"
+  }
 
-#   storage_os_disk {
-#     name              = "Clara-Campoamor-dsk"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Standard_LRS"
-#     disk_size_gb      = 100
-#   }
+  storage_os_disk {
+    name              = "Clara-Campoamor-dsk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = 100
+  }
 
-# os_profile {
-#     computer_name  = "Clara-Campoamor-PRE"
-#     admin_username = "webmaster-PRE"
-#     admin_password = "3ste0rdEnAdOresS3gRo"
-#   }
-#   os_profile_linux_config {
-#     disable_password_authentication = false
-#   }
-#   tags = {
-#     environment = "PRE"
-#   }
-# }
+os_profile {
+    computer_name  = "Clara-Campoamor-PRE"
+    admin_username = "webmaster-PRE"
+    admin_password = "3ste0rdEnAdOresS3gRo"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "PRE"
+  }
+}
 
-# #==============================================
-# #Creamos un servidor Ubuntu para la BBDD de PRE
-# #==============================================
-# resource "azurerm_public_ip" "Mary-Ritter-Beard-ip" {
-#   name                = "Mary-Ritter-Beard-ip"
-#   resource_group_name = local.rg_name
-#   location            = local.location
-#   allocation_method   = "Static"
-#   tags = {
-#     environment = "PRE"
-#   }
-# }
+#==============================================
+#Creamos un servidor Ubuntu para la BBDD de PRE
+#==============================================
+resource "azurerm_public_ip" "Mary-Ritter-Beard-ip" {
+  name                = "Mary-Ritter-Beard-ip"
+  resource_group_name = local.rg_name
+  location            = local.location
+  allocation_method   = "Static"
+  tags = {
+    environment = "PRE"
+  }
+}
 
-# #network interface ubuntu server pro
-# resource "azurerm_network_interface" "Mary-Ritter-Beard-interface" {
-#   name                = "Mary-Ritter-Beard-interface"
-#   location            = local.location
-#   resource_group_name = local.rg_name
+#network interface ubuntu server pro
+resource "azurerm_network_interface" "Mary-Ritter-Beard-interface" {
+  name                = "Mary-Ritter-Beard-interface"
+  location            = local.location
+  resource_group_name = local.rg_name
 
-#     ip_configuration {
-#         name = "internal"
-#         subnet_id = azurerm_subnet.subnet01-PRE.id
-#         private_ip_address_allocation = "Static"
-#         private_ip_address = "10.0.1.33"
-#         public_ip_address_id = azurerm_public_ip.Mary-Ritter-Beard-ip.id
-#   }
-# tags = {
-#     environment = "PRE"
-#   }
-# }
+    ip_configuration {
+        name = "internal"
+        subnet_id = azurerm_subnet.subnet01-PRE.id
+        private_ip_address_allocation = "Static"
+        private_ip_address = "10.0.1.33"
+        public_ip_address_id = azurerm_public_ip.Mary-Ritter-Beard-ip.id
+  }
+tags = {
+    environment = "PRE"
+  }
+}
 
-# #Despliegue de la máquina virtual de Ubuntu
-# resource "azurerm_virtual_machine" "Mary-Ritter-Beard" {
-#   name                  = "Mary-Ritter-Beard"
-#   location              = local.location
-#   resource_group_name   = local.rg_name
-#   network_interface_ids = [azurerm_network_interface.Mary-Ritter-Beard-interface.id]
-#   vm_size               = "Standard_B2as_v2"
+#Despliegue de la máquina virtual de Ubuntu
+resource "azurerm_virtual_machine" "Mary-Ritter-Beard" {
+  name                  = "Mary-Ritter-Beard"
+  location              = local.location
+  resource_group_name   = local.rg_name
+  network_interface_ids = [azurerm_network_interface.Mary-Ritter-Beard-interface.id]
+  vm_size               = "Standard_B2as_v2"
 
-# storage_image_reference {
-#    publisher = "canonical"
-#     offer     = "ubuntu-24_04-lts"
-#     sku       = "ubuntu-pro-gen1"
-#    version   = "latest"
-#   }
+storage_image_reference {
+   publisher = "canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "ubuntu-pro-gen1"
+   version   = "latest"
+  }
 
-#   storage_os_disk {
-#     name              = "Mary-Ritter-Beard-dsk"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Standard_LRS"
-#     disk_size_gb      = 100
-#   }
+  storage_os_disk {
+    name              = "Mary-Ritter-Beard-dsk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = 100
+  }
 
-# os_profile {
-#     computer_name  = "Mary-Ritter-Beard-PRE"
-#     admin_username = "BBDDadmin-PRE"
-#     admin_password = "C0nt1en3l4s8BDD"
-#   }
-#   os_profile_linux_config {
-#     disable_password_authentication = false
-#   }
-#   tags = {
-#     environment = "PRE"
-#   }
-# }
+os_profile {
+    computer_name  = "Mary-Ritter-Beard-PRE"
+    admin_username = "BBDDadmin-PRE"
+    admin_password = "C0nt1en3l4s8BDD"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "PRE"
+  }
+}
