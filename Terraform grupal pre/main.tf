e2402e18610b6a8ed149281738b77d4be5eebb14
LopedeVega22 <felteza@gmail.com>
2026-03-25 09:14:46 +0100

    Se corrige la IP del Alma Linux de PRE

--- Terraform grupal pre/main.tf
+++ Terraform grupal pre/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_network_interface" "alma-interface-PRE" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet01-PRE.id
     private_ip_address_allocation = "Static"
-    private_ip_address = "10.0.1.2"
+    private_ip_address = "10.0.1.12"
     public_ip_address_id = azurerm_public_ip.alma-ip-PRE.id
   }
   tags = {
